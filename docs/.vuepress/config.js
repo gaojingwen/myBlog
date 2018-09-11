558425ea902415c5b345df4ed47c562912237a47
断水流 <libao@shrise.cn>
2018-09-11 11:58:47 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,11 @@ module.exports = {
         ['meta', { charset: 'UTF-8' }],
         ['link', { rel: 'icon', href: '/favicon.ico' }] // 增加一个自定义的 favicon(网页标签的图标)
     ],
-    base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+    base: '/webApp/', // 这是部署到github相关的配置 下面会讲
     markdown: {
         lineNumbers: true // 代码块显示行号
     },
-    serviceWorker: true,
+    // serviceWorker: true,
     evergreen: true,
     themeConfig: {
         sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
